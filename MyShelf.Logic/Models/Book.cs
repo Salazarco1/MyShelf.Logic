073ea5a62036471c1babdf7eb1a31b8a670d88b3
COBY SALAZAR <COBY.SALAZAR8160@OMAIL.OTECH.EDU>
2025-06-03 11:50:36 -0600

    New Commit on 6/3/25

--- MyShelf.Logic/Models/Book.cs
+++ MyShelf.Logic/Models/Book.cs
@@ -8,5 +8,12 @@ namespace MyShelf.Logic.Models
 {
     public class Book
     {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public int PageCount { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+
     }
 }
